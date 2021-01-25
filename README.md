a8d0611a074b7afdffa707de2c5d36bccb697e78
Alex Korobko <korobko.alex@gmail.com>
2021-01-25 11:45:47 -0500

    Added a config example to Readme, improved error handling

--- README.md
+++ README.md
@@ -14,6 +14,20 @@ A client uses the utility instead of real server could experience various issues
  
  The server behaviour to request to any path on the service is configured in the `behavior` section of the config file.  Currently only one behaviour is supported, but as it covers my needs it is good enough. 
  
+ The following configuration  
+   ```
+  {
+    behaviour: configurable_response,
+    emulationport: "9000",
+    configurable_response: {
+        delay_before_millisec: 2000,
+    }
+  }
+  ```
+   
+  starts server listening to connections on port `9000`. The active behaviour is `configurable_response` that is configured after accepting a connection the service to wait for 2 sec and close the connection.
+  
+ 
  The following configuration
  
  ```
@@ -28,8 +42,9 @@ A client uses the utility instead of real server could experience various issues
 }
 ```
  
- starts sever listening to connections on port `9000`. The active behaviour is `configurable_response` that is configured to delay after accpting a connection for 100 milliseconds, then read content for the `response_sample2.txt` file in binary mode and send it via socket, then sleep for another `200` milliseconds and close the connection.
+ starts server listening to connections on port `9000`. The active behaviour is `configurable_response` that is configured to delay after accpting a connection for 100 milliseconds, then read content for the `response_sample2.txt` file in binary mode and send it via socket, then sleep for another `200` milliseconds and close the connection.
  As it was mentioned, the config could be changed, for example, modifying the delays or using another file and the changes will be applied to the next request to the service. The file is not cached, so any modification in the file also applied in the next request to the service.
+ 
  
  ##TODO 
  * to update the response that was loaded from file replacing by template some values, like current date and time
--- behaviours/configurable_response/configurable_response.go
+++ behaviours/configurable_response/configurable_response.go
@@ -14,33 +14,49 @@ const DELAY_AFTER_MILLISEC = "delay_after_millisec"
 
 func ConfigurableResponse(conn net.Conn, behaviourConfig map[string]interface{} ) {
 	if behaviourConfig != nil {
-		delayBeforeMillisec := behaviourConfig[DELAY_BEFORE_MILLISEC].(int)
-		if  delayBeforeMillisec > 0 {
-			time.Sleep(time.Duration(delayBeforeMillisec) * time.Millisecond)
-		}
-		responseFile := behaviourConfig[RESPONSE_FILE].(string)
-		if  len(responseFile) > 0 {
-			file, err := os.Open(responseFile)
-			if err != nil {
-				log.Fatal("Cannot open response file : " + err.Error())
+		configParam := behaviourConfig[DELAY_BEFORE_MILLISEC]
+		if configParam!= nil {
+			delayBeforeMillisec := configParam.(int)
+			if delayBeforeMillisec > 0 {
+				time.Sleep(time.Duration(delayBeforeMillisec) * time.Millisecond)
 			}
-			defer file.Close()
-
-			stats, statsErr := file.Stat()
-			if statsErr != nil {
-				log.Fatal("Cannot get size of response file : " + err.Error())
-			}
-			size := stats.Size()
-
-			bytes := make([]byte, size)
-			bufr := bufio.NewReader(file)
-			_,err = bufr.Read(bytes)
-			//TODO add replacing some values like current date by template
-			conn.Write(bytes)
 		}
-		delayAfterMillisec := behaviourConfig[DELAY_AFTER_MILLISEC].(int)
-		if  delayAfterMillisec > 0 {
-			time.Sleep(time.Duration(delayAfterMillisec) * time.Millisecond)
+
+		configParam = behaviourConfig[RESPONSE_FILE]
+		if configParam != nil {
+			responseFile := configParam.(string)
+			if len(responseFile) > 0 {
+				file, openFileErr := os.Open(responseFile)
+				if openFileErr != nil {
+					log.Fatal("Cannot open response file : " + openFileErr.Error())
+				}
+				defer file.Close()
+
+				stats, statsErr := file.Stat()
+				if statsErr != nil {
+					log.Fatal("Cannot get size of response file : " + statsErr.Error())
+				}
+				size := stats.Size()
+
+				bytes := make([]byte, size)
+				bufr := bufio.NewReader(file)
+				_, readBuffErr := bufr.Read(bytes)
+				if readBuffErr != nil {
+					log.Fatal("Cannot read the response file to buffer : " + readBuffErr.Error())
+				}
+				//TODO add replacing some values like current date by template
+				_, writeToSocketErr := conn.Write(bytes)
+				if writeToSocketErr != nil {
+					log.Fatal("Cannot read the response file to buffer : " + writeToSocketErr.Error())
+				}
+			}
+		}
+		configParam = behaviourConfig[DELAY_AFTER_MILLISEC]
+		if configParam != nil {
+			delayAfterMillisec := configParam.(int)
+			if delayAfterMillisec > 0 {
+				time.Sleep(time.Duration(delayAfterMillisec) * time.Millisecond)
+			}
 		}
 	}
 
